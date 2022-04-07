161a9931b952444fd03e16b595bd179e0f1b7595
Marvin Fette <fettema87760@th-nuernberg.de>
2022-04-07 14:51:08 +0200

    Add project files.

---
+++ Algorithmic Collection/Program.cs
@@ -0,0 +1,12 @@
+﻿using Algorithmic_Collection;
+
+namespace MainClass
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            Algorithem.MergeSort(new int[0]);
+        }
+    }
+}
---
+++ Algorithmic Testing Class/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithmic_Testing_Class
+{
+    [TestClass]
+    public class Algorithmic
+    {
+        [TestMethod]
+        public void MergeSort()
+        {
+        }
+    }
+}
