b6213259ef05af614b7a042e9c12479b8a1ad607
Marvin Fette <fettema87760@th-nuernberg.de>
2022-04-08 18:03:27 +0200

    Work on HeapSort

---
+++ Algorithmic Collection/HeapSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithmic_Collection
+{
+    public partial class Algorithem
+    {
+        private class HeapSortFunctions
+        {
+            private static int Parent(int i)
+            {
+                return i / 2;
+            }
+
+            private static int Left(int i)
+            {
+                return 2 * i;
+            }
+
+            private static int Right(int i)
+            {
+                return (i * 2) + 1;
+            }
+        }
+
+        public static void HeapSort(int[] a)
+        {
+
+        }
+    }
+}
--- Algorithmic Collection/MergeSort.cs
+++ Algorithmic Collection/MergeSort.cs
@@ -2,9 +2,47 @@
 {
     public partial class Algorithem
     {
+        private class MergeSortFunctions
+        {
+            public static void MergeSort(int[] a, int p, int r)
+            {
+                if (p < r)
+                {
+                    int q = (p + r) / 2;
+                    MergeSort(a, p, q);
+                    MergeSort(a, q + 1, r);
+                    Merge(a, p, q, r);
+                }
+            }
+
+            public static void Merge(int[] a, int p, int q, int r)
+            {
+                int n1 = q - p + 1;
+                int n2 = r - q;
+                int[] al = a[p..q];
+                int[] ar = a[(q + 1)..r];
+                al[n1 + 1] = int.MaxValue;
+                ar[n2 + 1] = int.MinValue;
+                int i = 0, j = 0;
+                for (int k = p; k <= r; k++)
+                {
+                    if (al[i] <= ar[j])
+                    {
+                        a[k] = al[i];
+                        i++;
+                    }
+                    else
+                    {
+                        a[k] = ar[j];
+                        j++;
+                    }
+                }
+            }
+        }
+
         public static void MergeSort(int[] a)
         {
-            
+            MergeSortFunctions.MergeSort(a, 0, a.Length);
         }
     }
 }
