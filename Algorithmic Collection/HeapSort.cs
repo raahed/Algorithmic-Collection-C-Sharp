10b30eb0ca70891400c30016742d2ad8c09bbc54
Marvin Fette <fettema87760@th-nuernberg.de>
2022-04-10 12:59:42 +0200

    Implement the Heap Sort algorithm

--- Algorithmic Collection/HeapSort.cs
+++ Algorithmic Collection/HeapSort.cs
@@ -4,19 +4,49 @@
     {
         private class HeapSortFunctions
         {
-            private static int Parent(int i)
+
+            public static int heapSize;
+
+            public static int Parent(int i)
             {
-                return i / 2;
+
+                return (i - 1) / 2;
             }
 
-            private static int Left(int i)
+            public static int Left(int i)
             {
-                return 2 * i;
+                return (2 * i) + 1;
             }
 
-            private static int Right(int i)
+            public static int Right(int i)
             {
-                return (i * 2) + 1;
+                return (2 * i) + 2;
+            }
+
+            public static void MaxHeapify(int[] a, int i)
+            {
+                int l = Left(i), r = Right(i), max;
+
+                if (l < heapSize && a[l] > a[i])
+                    max = l;
+                else
+                    max = i;
+
+                if (r < heapSize && a[r] > a[max])
+                    max = r;
+
+                if (max != i)
+                {
+                    (a[max], a[i]) = (a[i], a[max]);
+                    MaxHeapify(a, max);
+                }
+            }
+
+            public static void BuildMaxHeap(int[] a)
+            {
+                heapSize = a.Length;
+                for (int i = (a.Length / 2) - 1; i >= 0; i--)
+                    MaxHeapify(a, i);
             }
         }
 
@@ -24,6 +54,15 @@
         {
             if (a.Length < 2)
                 return;
+
+            HeapSortFunctions.BuildMaxHeap(a);
+
+            for (int i = a.Length - 1; i > 0; i--)
+            {
+                (a[0], a[i]) = (a[i], a[0]);
+                HeapSortFunctions.heapSize--;
+                HeapSortFunctions.MaxHeapify(a, 0);
+            }
         }
     }
 }
