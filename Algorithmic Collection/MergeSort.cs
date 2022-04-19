a6d1de753c4ef8d631002131050440736b548c65
Marvin Fette <fettema87760@th-nuernberg.de>
2022-04-19 16:38:15 +0200

    Add Mergesort without Sentinel

--- Algorithmic Collection/MergeSort.cs
+++ Algorithmic Collection/MergeSort.cs
@@ -14,6 +14,16 @@
                     Merge(a, p, q, r);
                 }
             }
+            public static void MergeSortWithoutSentinel(int[] a, int p, int r)
+            {
+                if (p < r)
+                {
+                    int q = (p + r) / 2;
+                    MergeSort(a, p, q);
+                    MergeSort(a, q + 1, r);
+                    MergeWithoutSentinel(a, p, q, r);
+                }
+            }
 
             public static void Merge(int[] a, int p, int q, int r)
             {
@@ -44,6 +54,55 @@
                     }
                 }
             }
+
+            public static void MergeWithoutSentinel(int[] a, int p, int q, int r)
+            {
+                int n1 = q - p + 1;
+                int n2 = r - q;
+
+                int[] al = new int[n1];
+                int[] ar = new int[n2];
+
+                Array.Copy(a[p..(q + 1)], al, n1);
+                Array.Copy(a[(q + 1)..(r + 1)], ar, n2);
+
+                int i = 0, j = 0;
+                for (int k = p; k <= r; k++)
+                {
+                    if (j == n2)
+                    {
+                        a[k] = al[i];
+                        i++;
+                    }
+                    else if (i == n1)
+                    {
+                        a[k] = ar[j];
+                        j++;
+                    }
+                    else
+                    {
+                        if (al[i] <= ar[j])
+                        {
+                            a[k] = al[i];
+                            i++;
+                        }
+                        else
+                        {
+                            a[k] = ar[j];
+                            j++;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public static void MergeSortWithoutSentinel(int[] a)
+        {
+            if (a.Length < 2)
+                return;
+
+            MergeSortFunctions.MergeSortWithoutSentinel(a, 0, a.Length - 1);
         }
 
         public static void MergeSort(int[] a)
